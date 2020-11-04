51903d7e128632f06ce0bb4d97d3ae1852317c88
Kotori <44732554+Lyrapuff@users.noreply.github.com>
2020-11-05 01:25:56 +0300

    Create quickreplies.user.js

---
+++ quickreplies.user.js
@@ -0,0 +1,89 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://my.digiseller.com/inside/purchases_inv_detail.asp?id_i=*&view=debate*
+// @grant        none
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let textbox = document.getElementsByClassName('form-textarea')[0];
+
+let container = document.getElementsByClassName('debates_content')[0].parentElement;
+container.style.display = 'flex';
+container.style.flexDirection = 'column';
+container.style.flexWrap = 'wrap';
+container.style.justifyContent = 'start';
+container.style.maxHeight = '636px';
+
+let chat = container.children[0];
+
+let typing = container.children[1];
+typing.style.maxWidth = '640px';
+
+let block = document.createElement('div');
+block.style.display = 'flex';
+block.style.flexDirection = 'column';
+block.style.justifyContent = 'start';
+block.style.height = '636px';
+
+let replies = localStorage.getItem('replies') ?? [
+    {
+        "short": `Дай код`,
+        "ru": `Пожалуйста, передайте код, полученный при покупке.`,
+        "eng": `Please, send me the code you received after purchasing.`
+    },
+    {
+        "short":  `Прими в друзья`,
+        "ru": `Пожалуйста, примите заявку в друзья.`,
+        "eng": `Please, accept the friend request.`
+    },
+    {
+        "short": `Нужно время`,
+        "ru": `Потребуется немного больше времени.`,
+        "eng": `It'll take some more time.`
+    },
+    {
+        "short": `Извитие`,
+        "ru": `Извините за доставленные неудобства.`,
+        "eng": `I'm sorry for this inconvenience.`
+    },
+    {
+        "short": `Спасибо за покупку`,
+        "ru": `Спасибо за покупку.\r\n Пожалуйста, оставьте отзыв.`,
+        "eng": `Thanks for purchashing.\r\n Please, leave a review.`
+    },
+
+];
+
+function setText(reply) {
+    textbox.value = reply.ru + "\r\n\r\n" + reply.eng;
+    textbox.dispatchEvent(new Event('input'));
+}
+
+replies.forEach(reply => {
+    let replyElement = document.createElement('div');
+    replyElement.style.cursor = 'pointer';
+    replyElement.style.backgroundColor = '#f6f8f9';
+    replyElement.style.border = '#cfd8dc 1px solid';
+    replyElement.style.fontSize = '16px';
+    replyElement.style.padding = '5px';
+    replyElement.style.borderRadius = '3px';
+    replyElement.style.fontFamily = 'Segoe UI';
+    replyElement.style.marginTop = '5px';
+    replyElement.style.width = '100%';
+    replyElement.innerHTML = reply.short;
+    replyElement.onclick = () => {
+        setText(reply);
+    };
+
+    block.appendChild(replyElement);
+});
+
+container.appendChild(block);
+})();
